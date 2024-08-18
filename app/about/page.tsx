335e8cbf3e99b391562ac89d710eac1e3ec28ab7
WitchElaina <msz13399173387@gmail.com>
2024-08-18 11:51:13 +0800

    🎉 init

---
+++ app/about/page.tsx
@@ -0,0 +1,34 @@
+import { Link } from "@nextui-org/link";
+import { button as buttonStyles } from "@nextui-org/theme";
+
+import { siteConfig } from "@/config/site";
+import { title, subtitle } from "@/components/primitives";
+import { GithubIcon } from "@/components/icons";
+
+export default function AboutPage() {
+  return (
+    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+      <div className="inline-block max-w-xl text-center justify-center">
+        <h1 className={title()}>Manage Your</h1>
+        <br />
+        <h1 className={title()}>Seasonly </h1>
+        <h1 className={title({ color: "violet" })}>Anime Watchlist</h1>
+        <br />
+        <h2 className={subtitle({ class: "mt-4" })}>
+          Beautiful, fast and modern anime watchlist manager.
+        </h2>
+      </div>
+
+      <div className="flex gap-3">
+        <Link
+          isExternal
+          className={buttonStyles({ variant: "bordered", radius: "full" })}
+          href={siteConfig.links.github}
+        >
+          <GithubIcon size={20} />
+          GitHub
+        </Link>
+      </div>
+    </section>
+  );
+}
